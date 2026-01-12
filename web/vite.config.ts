426d244bf5b41eb09acc56feb2519673c15c5c5a
Oridion <oridion.xyz@gmail.com>
2026-01-11 21:45:01 -0600

    Fixed base URL for GitHub pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 
-// GitHub Pages repo site is served at: https://<owner>.github.io/<repo>/
-// So base must be "/<repo>/" for assets to resolve correctly.
 export default defineConfig({
-    base: "/iseefortune-verifier/"
+    base: "./",
 });
